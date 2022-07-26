55431345fffb5a2dda6ffd1425bfac41e1d0059a
Rolleston <mr239@waikato.ac.nz>
2022-07-27 10:03:53 +1200

    .

---
+++ COMPX102/Week2/WordChallenge/WordChallenge/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WordChallenge
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //variable for value of word
+            int num = 0;
+            int count = 0;
+            //User input
+            Console.Write("Enter a word: ");
+            string word = Console.ReadLine().ToUpper();
+            var charArr = word.ToCharArray();
+           
+            //For loop to display chars using ascii values
+            for(int i = 65; i < 65+26; i++)
+            {
+                //Console.WriteLine(Convert.ToChar(i) + " = $" + Convert.ToString(i - 64));
+                if(Convert.ToChar(i) == )
+                {
+                    num = num + (i - 64);
+                    count++;
+                }
+            }
+
+            Console.WriteLine("Your word equals $" + num);
+            Console.WriteLine("-------------------------------------");
+            Console.WriteLine("Press enter to end...");
+            Console.ReadLine();
+        }
+    }
+}
