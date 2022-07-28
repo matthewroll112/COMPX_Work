2a7b1338e04eae36ef814ad2a64c098bb4dad88b
matthewroll112 <matthewroll112@gmail.com>
2022-07-28 18:08:22 +1200

    .

--- COMPX102/Week2/Ex3/Ex3/Program.cs
+++ COMPX102/Week2/Ex3/Ex3/Program.cs
@@ -1,3 +1,7 @@
+using System;
+using System.IO;
+using System.Text;
+
 namespace Ex3
 {
     internal static class Program
@@ -8,7 +12,48 @@ namespace Ex3
         [STAThread]
         static void Main()
         {
-            
+            //Variables
+            StreamReader reader;
+            string fileName = "names.txt";
+
+            List<string> names = new List<string>();
+            List<string> uNames = new List<string>();
+
+            string Line;
+
+            //Opening file
+            reader = File.OpenText(fileName);
+
+            //Loop until end of file
+            while (!reader.EndOfStream)
+            {
+                Line = reader.ReadLine();
+
+                if (!uNames.Contains(Line))
+                {
+                    uNames.Add(Line);
+                    names.Add(Line);
+                }
+                else
+                {
+                    names.Add(Line);
+                }
+            }
+
+            //grouping names
+            var grp = names.GroupBy(i => i);
+
+            //Counting and displaying eaach group
+            foreach(var gr in grp)
+            {
+                Console.WriteLine($"{gr.Key}\t{gr.Count()}");
+            }
+
+            //enter to quit
+            Console.WriteLine();
+            Console.WriteLine("---------------------");
+            Console.WriteLine("Press enter to exit...");
+            Console.ReadLine();
         }
     }
 }
